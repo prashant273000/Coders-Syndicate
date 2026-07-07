59ea8b38f773262f3400241dc143eff1473445f8
DjYash007 <thoratyashvardhan2008@gmail.com>
2026-07-07 19:38:55 +0530

    CORS fix

--- auth/backend/server.js
+++ auth/backend/server.js
@@ -188,7 +188,12 @@ admin.initializeApp({
 
 // Middleware
 app.use(cors(corsOptions));
-app.options("/*", cors(corsOptions));
+app.use((req, res, next) => {
+  if (req.method === "OPTIONS") {
+    return cors(corsOptions)(req, res, next);
+  }
+  next();
+});
 app.use(express.json());
 
 // Routes
